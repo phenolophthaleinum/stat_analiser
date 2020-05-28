785af7ee83905a8408dae848a2a0bd80de19ca3a
hyperscroll <mccv99@gmail.com>
2020-05-28 17:06:32 +0200

    Install packages

---
+++ analiser_package_install.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+
+install.packages(c("argparse", "dplyr", "Hmisc",
+                   "ggpubr", "stats", "car", "dunn.test", "FSA",
+                   "purrr", "reshape2", "stargazer"), repos = "http://cran.r-project.org")
